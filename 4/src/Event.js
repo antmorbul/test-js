6c426d76932419502eee39e81fe72150133e5209
antonio <antoniomorobullon@gmail.com>
2020-12-07 19:19:11 +0100

    create factory pattern

--- 4/src/Event.js
+++ 4/src/Event.js
@@ -1,3 +1,8 @@
 export default class Event{
-// implement your code here...
+  constructor(second, type, message) {
+    this.second = second;
+    this.type = type;
+    this.message = message;
+    console.log(`At second ${second}: ${JSON.stringify({type, message})}`);
+  }
 };
--- 4/src/EventManager.js
+++ 4/src/EventManager.js
@@ -1,6 +1,20 @@
-export default class EventManager{
+import Event from "./Event";
+
+export default class EventManager {
     run() {
-        // implement your code here...
-        console.log("RUNNING");
+        let timeCount = 0;
+        setInterval(() => {
+            this.events.forEach(event => {
+                if (event.second === timeCount && this.types.some(type => event.type === type)) {
+                    return new Event(event.second, event.type, event.message);
+                }
+            });
+            timeCount += 1;
+        }, 1000)
+    }
+
+    constructor(events, types) {
+        this.events = events;
+        this.types = types;
     }
 };
